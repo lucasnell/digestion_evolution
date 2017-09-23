b0d9516a1c3849c82d72d12c85fc9afccf9f4d64
lucasnell <lucas@lucasnell.com>
2017-09-23 12:06:00 -0500

    Added matlab directory

---
+++ matlab/code_v1/output_spp.m
@@ -0,0 +1,112 @@
+
+
+% @ METHOD == ML FOR ALL
+
+
+% @@@@@@@@
+% int_length_mass ~ taxon
+% @@@@@@@@
+
+% Coefficients with (lb,mean,ub) from simulation
+% b0 (intercept) = 7.735 (-1.8413, 7.6989, 16.8601)
+% b1 = -2.968 (-15.6563, -3.0539, 9.7208)
+% 
+% sigma2 = 7.5238 (3.1649, 8.0121, 14.9454)
+% 
+% Independent variable means, variances, and correlations
+% aX1 = 0.56235
+% s2X1 = 0.014909
+% 
+% LnLikelihood = -31.0981
+% -2LL = 62.1962
+% AIC(par=5) = 72.1962
+
+
+
+% @@@@@@@@
+% nsa_mass ~ taxon
+% @@@@@@@@
+
+% Coefficients with (lb,mean,ub) from simulation
+% b0 (intercept) = 1.4013 (-0.0034404, 1.3897, 2.6809)
+% b1 = -0.46295 (-2.3317, -0.45387, 1.4062)
+% 
+% sigma2 = 0.16362 (0.058143, 0.16779, 0.31562)
+% 
+% Independent variable means, variances, and correlations
+% aX1 = 0.56235
+% s2X1 = 0.014909
+% 
+% LnLikelihood = 3.0782
+% -2LL = -6.1564
+% AIC(par=5) = 3.8436
+
+
+% @@@@@@@@
+% vill_area_mass ~ taxon
+% @@@@@@@@
+
+% Coefficients with (lb,mean,ub) from simulation
+% b0 (intercept) = 13.0269 (-6.7917, 13.301, 33.3597)
+% b1 = -0.36495 (-26.7427, -0.70089, 27.2176)
+% 
+% sigma2 = 31.732 (14.2734, 34.5016, 63.5114)
+% 
+% Independent variable means, variances, and correlations
+% aX1 = 0.56235
+% s2X1 = 0.014909
+% 
+% LnLikelihood = -44.046
+% -2LL = 88.092
+% AIC(par=5) = 98.092
+
+
+% @@@@@@@@
+% log_total_enterocytes ~ taxon + log_mass
+% @@@@@@@@
+
+% Coefficients with (lb,mean,ub) from simulation
+% b0 (intercept) = 17.2073 (15.2167, 17.2069, 19.1215)
+% b1 = 0.10643 (-0.36704, 0.11143, 0.59087)
+% b2 = 0.7761 (0.39555, 0.7768, 1.1836)
+% 
+% sigma2 = 0.27675 (0.12195, 0.36264, 0.69749)
+% 
+% Independent variable means, variances, and correlations
+% aX1 = 0.56806
+% aX2 = 3.4675
+% s2X1 = 0.014911
+% s2X2 = 0.55527
+% r(1,2) = -0.16246
+% 
+% LnLikelihood = -21.9863
+% -2LL = 43.9725
+% AIC(par=8) = 59.9725
+
+
+
+% @@@@@@@@
+% absorp ~ taxon
+% @@@@@@@@
+
+% Coefficients with (lb,mean,ub) from simulation
+% b0 (intercept) = 0.014718 (-0.038165, 0.015715, 0.068382)
+% b1 = 0.034023 (-0.051785, 0.032482, 0.11686)
+% 
+% sigma2 = 2.5211e-21 (2.963e-23, 9.4939e-21, 5.4164e-20)
+% 
+% Independent variable means, variances, and correlations
+% aX1 = 0.54995
+% s2X1 = 0.065661
+% 
+% LnLikelihood = 26.0585
+% -2LL = -52.117
+% AIC(par=5) = -42.117
+
+
+
+% @@@@@@@@
+% sef ~ dietOmnivorous + dietProtein
+% @@@@@@@@
+
+
