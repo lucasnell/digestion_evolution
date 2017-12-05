7095e88eefe49b71af39b13d016bba37c71fbe0f
Lucas Nell <lucas@lucasnell.com>
2017-12-05 13:43:57 -0600

    Removed, ignoring running directory

--- running/update_running.R
+++
@@ -1,9 +0,0 @@
-library(knitr)
-# This is to update the `running` directory
-# You can ignore this file if you've only been sent this directory
-purl('doc/phylo_regressions.Rmd', 'running/phylo_regressions.R')
-file.copy('R', 'running', recursive = TRUE)
-file.copy('data/tree.nwk', 'running/data', recursive = TRUE)
-for (f in list.files('output', 'tidy_*', full.names = TRUE)) {
-    file.copy(f, to = 'running/output', overwrite = TRUE)
-}; rm(f)
